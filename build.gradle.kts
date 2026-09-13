eedcf31908f5cec6a3087074beef5eb1ce12390b
JoshRiang <joshuariangkamang@gmail.com>
2026-09-13 17:57:43 +0700

    Fix: KSP version 2.1.0-1.0.24 (2.1.0-1.0.25 doesn't exist)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.25" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.24" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
